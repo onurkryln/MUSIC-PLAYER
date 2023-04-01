3b7470afc19157cf4f4c4a8b0a06112334178809
Onur karayılan <onurkryln2323@gmail.com>
2023-04-02 00:47:18 +0300

    C# Music player

---
+++ Solution3/MusicPlayer/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace MusicPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.browse = new System.Windows.Forms.Button();
+            this.play = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.pause = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.geri = new System.Windows.Forms.Button();
+            this.ileri = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // browse
+            // 
+            this.browse.BackColor = System.Drawing.Color.Ivory;
+            this.browse.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.browse.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.browse.Location = new System.Drawing.Point(235, 171);
+            this.browse.Margin = new System.Windows.Forms.Padding(4);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(65, 30);
+            this.browse.TabIndex = 1;
+            this.browse.Text = "browse";
+            this.browse.UseVisualStyleBackColor = false;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // play
+            // 
+            this.play.BackColor = System.Drawing.Color.Ivory;
+            this.play.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.play.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.play.Location = new System.Drawing.Point(318, 171);
+            this.play.Margin = new System.Windows.Forms.Padding(4);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(65, 30);
+            this.play.TabIndex = 2;
+            this.play.Text = "play";
+            this.play.UseVisualStyleBackColor = false;
+            this.play.Click += new System.EventHandler(this.play_Click);
+            // 
+            // stop
+            // 
+            this.stop.BackColor = System.Drawing.Color.Ivory;
+            this.stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.stop.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.stop.Location = new System.Drawing.Point(391, 171);
+            this.stop.Margin = new System.Windows.Forms.Padding(4);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(65, 30);
+            this.stop.TabIndex = 3;
+            this.stop.Text = "stop";
+            this.stop.UseVisualStyleBackColor = false;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // pause
+            // 
+            this.pause.BackColor = System.Drawing.Color.Ivory;
+            this.pause.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.pause.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.pause.Location = new System.Drawing.Point(464, 171);
+            this.pause.Margin = new System.Windows.Forms.Padding(4);
+            this.pause.Name = "pause";
+            this.pause.Size = new System.Drawing.Size(65, 30);
+            this.pause.TabIndex = 4;
+            this.pause.Text = "pause";
+            this.pause.UseVisualStyleBackColor = false;
+            this.pause.Click += new System.EventHandler(this.pause_Click);
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.Color.Ivory;
+            this.exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.exit.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.exit.Location = new System.Drawing.Point(537, 171);
+            this.exit.Margin = new System.Windows.Forms.Padding(4);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(65, 30);
+            this.exit.TabIndex = 5;
+            this.exit.Text = "exit";
+            this.exit.UseVisualStyleBackColor = false;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(1, 168);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(113, 33);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(131, 168);
+            this.hScrollBar1.Maximum = 109;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(80, 33);
+            this.hScrollBar1.TabIndex = 12;
+            this.hScrollBar1.Value = 10;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.listBox1.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Items.AddRange(new object[] {
+            ""});
+            this.listBox1.Location = new System.Drawing.Point(1, -3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(688, 144);
+            this.listBox1.TabIndex = 13;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Player
+            // 
+            this.Player.Enabled = true;
+            this.Player.Location = new System.Drawing.Point(1, 1);
+            this.Player.Margin = new System.Windows.Forms.Padding(4);
+            this.Player.Name = "Player";
+            this.Player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Player.OcxState")));
+            this.Player.Size = new System.Drawing.Size(688, 200);
+            this.Player.TabIndex = 0;
+            this.Player.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // geri
+            // 
+            this.geri.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.geri.Location = new System.Drawing.Point(611, 179);
+            this.geri.Name = "geri";
+            this.geri.Size = new System.Drawing.Size(25, 22);
+            this.geri.TabIndex = 15;
+            this.geri.Text = "<";
+            this.geri.UseVisualStyleBackColor = true;
+            this.geri.Click += new System.EventHandler(this.geri_Click);
+            // 
+            // ileri
+            // 
+            this.ileri.Location = new System.Drawing.Point(642, 179);
+            this.ileri.Name = "ileri";
+            this.ileri.Size = new System.Drawing.Size(26, 22);
+            this.ileri.TabIndex = 16;
+            this.ileri.Text = ">";
+            this.ileri.UseVisualStyleBackColor = true;
+            this.ileri.Click += new System.EventHandler(this.ileri_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(688, 202);
+            this.Controls.Add(this.ileri);
+            this.Controls.Add(this.geri);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.pause);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.play);
+            this.Controls.Add(this.browse);
+            this.Controls.Add(this.Player);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private AxWMPLib.AxWindowsMediaPlayer Player;
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.Button play;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button pause;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button geri;
+        private System.Windows.Forms.Button ileri;
+    }
+}
+
---
+++ Solution3/MusicPlayer/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace MusicPlayer
+{
+    public partial class Form1 : Form
+    {
+        List<string> playlist;
+        int count;
+        public Form1()
+        {
+            playlist = new List<string>();
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                foreach (string fileName in openFileDialog1.FileNames)
+                {
+                    playlist.Add(fileName);
+                    listBox1.Items.Add(fileName);
+                }
+                    string name= openFileDialog1.FileName;
+                textBox1.Text= name.Substring(26);
+
+            }
+            Player.URL = openFileDialog1.FileName;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void play_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != 0) {
+                openFileDialog1.FileName = listBox1.SelectedItem.ToString();
+                Player.URL = openFileDialog1.FileName;
+                Player.Ctlcontrols.play();
+                string name = openFileDialog1.FileName;
+                textBox1.Text = name.Substring(26);
+            }
+
+            Player.Ctlcontrols.play();
+
+
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            Player.Ctlcontrols.stop();
+        }
+
+        private void pause_Click(object sender, EventArgs e)
+        {
+            Player.Ctlcontrols.pause();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+      
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            Player.settings.volume = e.NewValue;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void hScrollBar2_Scroll(object sender, ScrollEventArgs e)
+        {
+            
+        }
+
+        private void geri_Click(object sender, EventArgs e)
+        {
+            int count = 0;int a = 0;
+            foreach (var items in listBox1.Items)
+            {
+                count++;
+                if (Player.URL == items.ToString())
+                {
+
+                    if ( count!=a)
+                    {
+                        int sa = count-2;
+
+                        Player.URL = listBox1.Items[sa].ToString();
+                        Player.Ctlcontrols.play();
+                        textBox1.Text = Player.URL;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void ileri_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            foreach (var items in listBox1.Items)
+            {
+                count++;
+                if (Player.URL == items.ToString())
+                {
+
+                    if (listBox1.Items.Count > count)
+                    {
+                        int sa = count ;
+
+                        Player.URL = listBox1.Items[sa].ToString();
+                        Player.Ctlcontrols.play();
+                        textBox1.Text = Player.URL;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+    
